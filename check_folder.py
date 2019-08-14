aded78826c5ca3b8033ac15c33675dd750087865
Andy Martin <in_ex@xyz.de>
2019-08-14 18:49:43 +0200

    Update check_folder.py

--- check_folder.py
+++ check_folder.py
@@ -1,5 +1,9 @@
 import os
 import argparse
+from colorama import init, Fore, Back, Style
+
+# Initialize colorama
+init()
 
 parser = argparse.ArgumentParser(description='Check for non image files in input folder and move them to output folder.')
 parser.add_argument('input_folder', type=str, help='defines the folder that is searched')
@@ -12,6 +16,14 @@ output_folder = args.output_folder
 
 files = []
 extensions = ['.jpg','.jpeg','.png','.cr2','.bmp','.tif','.raw','.gif']
+
+
+print(Fore.BLUE + "Input folder: " + Fore.GREEN + input_folder)
+print(Fore.BLUE + "Output folder: " + Fore.GREEN + output_folder)
+print("")
+print(Fore.GREEN + "Checking for files...")
+
+# Check all files on input_folder
 # r=root, d=directories, f = files
 for r, d, f in os.walk(input_folder):
     for file in f:
@@ -21,8 +33,22 @@ for r, d, f in os.walk(input_folder):
         else:
             files.append(os.path.join(r, file))
 
+print(str(len(files)) + " files will be moved...")
+print("")
+
+# determine output_path and where it matches
+path_parts_input = input_folder.split('\\')
+path_parts_output = output_folder.split('\\')
+min_len_paths = min(len(path_parts_input), len(path_parts_output))
+path_start = 1
+
+for i in range(min_len_paths):
+    if path_parts_input[i] == path_parts_output[i]:
+        path_start += 1
+
+# move found files
 for f in files:
-    file_parts = f.split('\\')[1:-1]
+    file_parts = f.split('\\')[path_start:-1]
     file_folder = '\\'.join(file_parts)
     new_folder = output_folder + '\\' + file_folder
     create_path = ''
@@ -36,7 +62,7 @@ for f in files:
         if not os.path.isdir(create_path):
             os.mkdir(create_path)
     
-    new_file_path = output_folder + '\\' + '\\'.join(f.split('\\')[1:])
+    new_file_path = output_folder + '\\' + '\\'.join(f.split('\\')[path_start:])
     os.rename(f, new_file_path)
 
 print('Files were moved!')
